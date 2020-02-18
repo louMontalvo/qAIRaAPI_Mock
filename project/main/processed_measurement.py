30dd17e6f2196d6e9a4febdb21e5f31378871250
louMontalvo <l.montalvo@qairadrones.com>
2020-02-18 18:44:45 +0100

    update data processed and download processed

--- project/main/processed_measurement.py
+++ project/main/processed_measurement.py
@@ -43,6 +43,10 @@ def handleProcessedData():
         data_json = request.get_json()
         product_id = data_json['ID']
         utils.storeProcessedDataInDB(db.session, data_json)
+
+        data_json['timestamp'] = str(data_json['timestamp'])
+        data_json['ID'] = product_id
+
         socketio.emit('new_data_event_processed', data_json, room=product_id)
         socketio.emit('new_data_summary_processed', data_json)
         return make_response('OK', 200)
@@ -67,8 +71,8 @@ def getProcessedMeasurementsLastRows():
     return make_response(jsonify('Measurements not found'), 404)
 
 
-@app.route('/api/processed_measurements_period/', methods=['GET'])
-def getProcessedMeasurementsTimePeriod():
+@app.route('/api/average_processed_measurements_period/', methods=['GET'])
+def getAverageProcessedMeasurementsTimePeriod():
     qhawax_name = request.args.get('name')
     initial_timestamp = dateutil.parser.parse(request.args.get('initial_timestamp'))
     final_timestamp = dateutil.parser.parse(request.args.get('final_timestamp'))
@@ -78,4 +82,16 @@ def getProcessedMeasurementsTimePeriod():
         processed_measurements_list = [measurement._asdict() for measurement in processed_measurements]
         averaged_measurements_list = utils.averageMeasurementsInHours(processed_measurements_list, initial_timestamp, final_timestamp, 1)
         return make_response(jsonify(averaged_measurements_list), 200)
+    return make_response(jsonify('Measurements not found'), 404)
+
+@app.route('/api/processed_measurements_period/', methods=['GET'])
+def getProcessedMeasurementsTimePeriod():
+    qhawax_name = request.args.get('name')
+    initial_timestamp = dateutil.parser.parse(request.args.get('initial_timestamp'))
+    final_timestamp = dateutil.parser.parse(request.args.get('final_timestamp'))
+    processed_measurements = utils.queryDBProcessed(db.session, qhawax_name, initial_timestamp, final_timestamp)
+
+    if processed_measurements is not None:
+        processed_measurements_list = [measurement._asdict() for measurement in processed_measurements]
+        return make_response(jsonify(processed_measurements_list), 200)
     return make_response(jsonify('Measurements not found'), 404)
