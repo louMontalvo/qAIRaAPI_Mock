373a57465de1172818baa5bbabe7289d6308970d
louMontalvo <l.montalvo@qairadrones.com>
2020-03-04 17:06:27 -0500

    user login

--- project/__init__.py
+++ project/__init__.py
@@ -21,7 +21,7 @@ login_manager.init_app(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 
-from project.main import gas_sensor, processed_measurement, qhawax, air_quality, raw_measurement,eca_noise, gas_inca, qhawax_installation_history
+from project.main import gas_sensor, processed_measurement, qhawax, air_quality, raw_measurement,eca_noise, gas_inca, qhawax_installation_history, user
 import project.database.models as models
 from project.database.models import Company, User, Qhawax , ProcessedMeasurement, AirQualityMeasurement, RawMeasurement, EcaNoise, GasInca, QhawaxInstallationHistory
 
---
+++ project/main/user.py
@@ -0,0 +1,41 @@
+ from flask_login import current_user, login_required, login_user, logout_user
+from flask import jsonify, make_response, redirect, render_template, \
+    request, url_for
+import datetime
+import dateutil.parser
+import dateutil.tz
+import os
+
+from project import app, db
+from project.database.models import User, Company
+import project.main.utils as utils
+from sqlalchemy import or_
+
+import base64
+
+@app.route('/api/login/', methods=['GET'])
+def login():
+	email = request.args.get('email')
+	password = request.args.get('password')
+	print(password)
+	user = db.session.query(User).filter_by(email=email).first()
+	if user and user.validatePassword(password):
+		#Dame el nombre y id del usuario
+		username = email[:email.find('@')]
+		user_id = user.id
+		company_id = user.company_id
+		#Dame el nombre y id de la compania del usuario
+		company_name= db.session.query(Company.name).filter_by(id=company_id).first()
+		user_data={}
+		user_data['username']=username
+		user_data['user_id']=user_id
+		user_data['company_name']=company_name[0]
+		user_data['company_id']=company_id
+		return make_response(jsonify(user_data), 200)
+	else:
+		return make_response(jsonify('User and password not valid'), 404)
+
+
+
+
+
